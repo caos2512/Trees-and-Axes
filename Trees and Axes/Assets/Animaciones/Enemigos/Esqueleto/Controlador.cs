38e53b3ce4991e5ecae4488c19efdbaecd7eacdf
caos2512 <diamond.skull2512@gmail.com>
2024-10-10 22:35:09 -0600

    Summary

--- Trees and Axes/Assets/Animaciones/Enemigos/Esqueleto/Controlador.cs
+++ Trees and Axes/Assets/Animaciones/Enemigos/Esqueleto/Controlador.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class Controlador : MonoBehaviour
 {
-    public Transform Jugador;
-    public float deteccion = 5.0f;
-    public float speed = 2.0f;
-
-    private Rigidbody2D rigidBody;
-    private Vector2 movement;
-    // Start is called before the first frame update
     void Start()
     {
         
